06cc81f2dd09d06e1abdde25a1ba5aa83d88402d
unknown <crismai.dev@gmail.com>
2023-07-11 14:24:45 -0300

    Criando rota de login

--- APIRest/api/routes/index.js
+++ APIRest/api/routes/index.js
@@ -1,7 +1,7 @@
 const bodyParser = require('body-parser')
- 
 const produto = require('./produtoRoute')
 const usuario = require('./usuariosRoute')
+const auth = require('./authRoute')
 
 module.exports = app => {
   app.use(
