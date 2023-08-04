0c4d4be1ab0ef1ef66ce26351056199c7c4338fe
unknown <crismai.dev@gmail.com>
2023-08-04 16:45:18 -0300

    Cadastrando permissões para os perfis

--- APIRest/api/routes/seguranca.js
+++ APIRest/api/routes/seguranca.js
@@ -5,6 +5,8 @@ const router = Router()
 
 router
     .post('/seguranca/acl', SegurancaController.cadastrarAcl)
+    .post('/seguranca/permissoes-roles', SegurancaController.cadastrarPermissoesRoles)
+    
 
     
 
