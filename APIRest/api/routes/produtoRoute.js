efa66ffa9313f1f95604938a0b5e85bd51778aac
unknown <crismai.dev@gmail.com>
2023-08-08 13:48:26 -0300

    Implementando middleware permissões

--- APIRest/api/routes/produtoRoute.js
+++ APIRest/api/routes/produtoRoute.js
@@ -1,12 +1,13 @@
 const { Router } = require('express')
 const ProdutoController = require('../controllers/produtoController')
 const roles = require('../middleware/roles')
+const permissoes = require('../middleware/permissoes')
 
 const router = Router()
 
 router
   .post('/produto', ProdutoController.cadastrarProduto)
-  .get('/produto', roles(["Gerente", "Vendedor"]), ProdutoController.buscarTodosProdutos)
+  .get('/produto', permissoess(["listar"]), ProdutoController.buscarTodosProdutos)
   .get('/produto/id/:id', ProdutoController.buscarProdutoPorId)
   .delete('/produto/id/:id', ProdutoController.deletarProdutoPorId)
   .put('/produto/id/:id', ProdutoController.editarProduto)
